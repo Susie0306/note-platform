5becdd8063771beac4aa1a4fe066c8ea253e118f
Susie0306 <2934932480@qq.com>
2025-12-10 22:34:23 +0800

    修复一个bug

--- components/ui/block-toolbar-button.tsx
+++ components/ui/block-toolbar-button.tsx
@@ -15,7 +15,8 @@ export function BlockToolbarButton({ type, children, tooltip }: BlockToolbarButt
   const editor = useEditorRef()
   const onClick = () => {
     if (!editor) return
-    Transforms.setNodes(editor, { type }, { match: (n) => Editor.isBlock(editor, n) })
+    // platejs 的 editor 类型与 slate BaseEditor 存在类型不兼容，这里做一次宽松转换
+    Transforms.setNodes(editor as any, { type }, { match: (n) => Editor.isBlock(editor as any, n) })
   }
   return (
     <ToolbarButton onClick={onClick} tooltip={tooltip}>
